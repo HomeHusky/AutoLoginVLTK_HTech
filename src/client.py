4e58bfad4f8c3aea10aa1e37fff4125fd421947d
HomeHusky <vitrannhat1@gmail.com>
2024-12-22 17:32:29 +0700

    1.1.3 Update send data from clients

--- src/client.py
+++ src/client.py
@@ -19,33 +19,26 @@ def send_data(total_income, low_income_accounts, check_time, kpi_value, car_list
     # Dữ liệu cần gửi
     data = {
         "workstation_id": workstation_id,
-        "accounts": total_income,
-        "low_income_accounts": low_income_accounts,
+        "accounts": [{"account": acc[0], "quantity": acc[1]} for acc in total_income],
+        "low_income_accounts": [{"account": acc[0], "income": acc[1]} for acc in low_income_accounts],
         "check_time": check_time,
         "kpi_value": kpi_value,
         "car_list": car_list,
         "time_loop_send": time_loop_send,
-        # "accounts": [
-        #     {"account": "Account1", "quantity": 100},
-        #     {"account": "Account2", "quantity": 200},
-        #     {"account": "Account3", "quantity": 150},
-        # ]
     }
 
     print("data: ", data)
 
     try:
-        response = requests.post(url, json=data)
-        response.raise_for_status()  # Tự động tạo ngoại lệ nếu lỗi xảy ra
-        print("Gửi thành công, phản hồi:", response.json())
-    except requests.exceptions.HTTPError as errh:
-        print("Lỗi HTTP:", errh)
-    except requests.exceptions.ConnectionError as errc:
-        print("Lỗi Kết Nối:", errc)
-    except requests.exceptions.Timeout as errt:
-        print("Lỗi Timeout:", errt)
-    except requests.exceptions.RequestException as err:
-        print("Lỗi khác:", err)
+        # Gửi yêu cầu POST đến server
+        response = requests.post(url, json=data, allow_redirects=False)
+        if response.status_code == 200:
+            print("Gửi dữ liệu thành công.")
+            print("Phản hồi từ server:", response.json())
+        else:
+            print(f"Lỗi: {response.status_code} - {response.text}")
+    except Exception as e:
+        print("Lỗi khi gửi dữ liệu:", e)
 
 if __name__ == "__main__":
     send_data()
