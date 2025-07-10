9d2ab192585daf5911ac677fccf3a4023e24a31e
Tran Nhat Vi <vitrannhat1@gmail.com>
2025-07-10 19:02:38 +0700

    1.2.3 Fix bug

--- src/startLogin.py
+++ src/startLogin.py
@@ -112,13 +112,11 @@ def auto_login(account, sleepTime, currentAutoName, isAutoClickVLBS, isChangeSer
     # pyautogui.press('enter')
 
     working_dir = os.path.dirname(account['game_path'])
-    all_pids = []
-    all_pids = GF.get_all_vpid_vo_lam_windows()
-    base_length = len(all_pids)
+    
     print(f"Base length of VLTK windows: {base_length}")
 
     for attempt in range(3):
-        
+        base_length = len(GF.get_all_vpid_vo_lam_windows())
         subprocess.Popen(account['game_path'], cwd=working_dir)
         time.sleep(sleepTime[0]['wait_time_open'])
         new_length = len(GF.get_all_vpid_vo_lam_windows())
