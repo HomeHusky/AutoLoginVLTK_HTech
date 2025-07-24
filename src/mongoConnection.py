4930e09d1f0dbce6b4baf23422fd599a1a270f97
Tran Nhat Vi <vitrannhat1@gmail.com>
2025-07-25 00:51:12 +0700

    1.5.5 Try to remove srv connection

--- src/mongoConnection.py
+++ src/mongoConnection.py
@@ -12,8 +12,10 @@ def connect_mongo(uri=None, db_name="HtechVolam", collection_name="tai_khoan_may
     """
     if uri is None:
         # URI mặc định - bạn nên thay bằng URI thật
-        uri = "mongodb+srv://htechvolam:Htech317@htechvolam.oefc26z.mongodb.net/?retryWrites=true&w=majority&appName=HtechVolam"
-    
+        # uri = "mongodb+srv://htechvolam:Htech317@htechvolam.oefc26z.mongodb.net/?retryWrites=true&w=majority&appName=HtechVolam"
+        uri = "mongodb://htechvolam:Htech317@htechvolam-shard-00-00.oefc26z.mongodb.net:27017,htechvolam-shard-00-01.oefc26z.mongodb.net:27017,htechvolam-shard-00-02.oefc26z.mongodb.net:27017/?ssl=true&replicaSet=atlas-8v6xvn-shard-0&authSource=admin&retryWrites=true&w=majority"
+        
+
     client = MongoClient(uri, tlsCAFile=certifi.where())
     db = client[db_name]
     collection = db[collection_name]
